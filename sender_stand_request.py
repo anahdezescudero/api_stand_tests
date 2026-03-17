fb5f8c5d64e8373cb5a0432ec956fc531099716b
anakaren hernandez escudero <anitahdezes@gmail.com>
2026-03-17 10:39:03 -0600

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,24 @@
+import configuration
+import requests
+import data
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+response = post_new_user(data.user_body);
+print(response.status_code)
+print(response.json())
+
+
+
+
+
+
